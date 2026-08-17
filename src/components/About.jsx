c415129dfd0b0d16e4d873e3fa22124c77f2b908
Marccel Janara <nengahyuichiro34@gmail.com>
2026-08-17 20:29:09 +0700

    feat: add About section component with profile image and skill list

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
               <div className="aspect-square bg-slate-700 flex items-center justify-center text-slate-500">
                 <div className="text-center">
                   <p className="text-sm">
-                    <img src="photo_profile2.jpeg" alt="photo_profile" srcSet="" />
+                    <img src="photo_profile.jpeg" alt="photo_profile" srcSet="" />
                   </p>
                 </div>
               </div>
